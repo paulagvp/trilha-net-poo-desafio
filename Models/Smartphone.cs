25acd3b6aaf7fb1eec2e85716cbe4795be6b7a1d
paulagvp <paulagvp@gmail.com>
2024-01-07 14:23:50 -0300

    Entrega do desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,24 +3,29 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
         public Smartphone(string numero)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone (string modelo, string imei, int memoria)
+        {
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia(numero: "21212121212", modelo: "Modelo 1", imei: "12121212121", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine(" ");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone (numero: "3333333333", modelo: "Modelo 2", imei: "55555555555", memoria: 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
