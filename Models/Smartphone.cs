6b07ff711d79d06ca253fb3bd224ea85849e69e3
Paula <paulagvp@gmail.com>
2024-01-26 12:21:59 -0300

    OK

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,4 +1,5 @@
 namespace DesafioPOO.Models
+
 {
     public abstract class Smartphone
     {
--- Program.cs
+++ Program.cs
@@ -1,12 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
+
 Console.WriteLine("Nokia");
 Smartphone nokia = new Nokia(numero: "21212121212", modelo: "Modelo 1", imei: "12121212121", memoria: 32);
 nokia.Ligar();
+nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Telegram");
 
 Console.WriteLine(" ");
 
 Console.WriteLine("Iphone");
 Smartphone iphone = new Iphone (numero: "3333333333", modelo: "Modelo 2", imei: "55555555555", memoria: 128);
-nokia.Ligar();
-nokia.InstalarAplicativo("Telegram");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
